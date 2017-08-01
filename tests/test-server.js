49fea8cf4d17f227351b0fddff0a4d0e41054339
Katia <katia.ashkar@gmail.com>
2017-08-01 17:34:04 +0300

    create file travis.yml relate #7

--- tests/test-server.js
+++ tests/test-server.js
@@ -2,14 +2,14 @@ const test = require('tape');
 const shot = require('shot');
 const router = require('../src/router.js');
 
-test('Home Router', (t) =>{
+test('Home Router', (t) => {
 	shot.inject(router, {method: 'GET', url: '/'}, (response) => {
 		t.equal(response.statusCode, 200, 'Should respond with status code 200');
 		t.end();
 	})
 })
 
-test('Unknow uri', (t) =>{
+test('Unknow uri', (t) => {
 	shot.inject(router, {method: 'GET', url: '/lumos'}, (response) => {
 		t.equal(response.statusCode, 404, 'Should respond with status code 404');
 		t.equal(response.payload, '<h1>404 file not found</h1>', 'Should return 404 not found');
@@ -17,21 +17,21 @@ test('Unknow uri', (t) =>{
 	})
 })
 
-test('test for style.css file', (t) =>{
+test('test for style.css file', (t) => {
 	shot.inject(router, {method: 'GET', url: '/public/style.css'}, (response) => {
 		t.equal(response.statusCode, 200, 'Should respond with status code 200');
 		t.end();
 	})
 })
 
-test('test for reset.css file', (t) =>{
+test('test for reset.css file', (t) => {
 	shot.inject(router, {method: 'GET', url: '/public/reset.css'}, (response) => {
 		t.equal(response.statusCode, 200, 'Should respond with status code 200');
 		t.end();
 	})
 })
 
-test('test for index.js file', (t) =>{
+test('test for index.js file', (t) => {
 	shot.inject(router, {method: 'GET', url: '/public/index.js'}, (response) => {
 		t.equal(response.statusCode, 200, 'Should respond with status code 200');
 		t.end();
